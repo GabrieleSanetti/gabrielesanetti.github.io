557102d8b0414cda0ed82271b7186f3daefca37c
GabrieleSanetti <133086280+GabrieleSanetti@users.noreply.github.com>
2025-08-30 13:37:53 +0200

    Created and linked files JS for EmailJS

---
+++ assets/js/emailjs-connector.js
@@ -0,0 +1,6 @@
+(function () {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init({
+    publicKey: "Db89jDNYE2ve9513w",
+  });
+})();
---
+++ assets/js/emailjs-form-connector.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      // these IDs from the previous steps
+      emailjs.sendForm("gmail", "contact_form", this).then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error);
+        }
+      );
+    });
+};
